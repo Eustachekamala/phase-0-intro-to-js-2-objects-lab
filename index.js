d1c618eb04b1b6bfd24e15a20347c2701bbc2610
eustachekamala <eustachejared25@gmail.com>
2024-07-05 20:31:52 +0300

    I created some methods that they to modify the object emplayee, for exemple to add employee without change object, remove an employee element without change the initial elelemt of the object,...

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+
+let employee = {
+    name:'Eustache',
+    streetAddress:'River-road Num:23',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key , value){
+
+    const newObject = {...employee,
+        name : 'Sam',
+        streetAddress : '11 Broadway',  
+    }
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    employee[name] = streetAddress;
+    employee["streetAddress"] = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const deleteEmployee = {...employee};
+    delete deleteEmployee[key];
+    return deleteEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    let newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee,'name');
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+updateEmployeeWithKeyAndValue();
+destructivelyUpdateEmployeeWithKeyAndValue();
+deleteFromEmployeeByKey();
+destructivelyDeleteFromEmployeeByKey();
